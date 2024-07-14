f2b54d47f7a718cc3a93e33de2d5cc36287921b2
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-07-14 16:11:02 -0600

    reflect proper starter code

---
+++ generateMarkdown.js
@@ -0,0 +1,43 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (!license) {
+      return '';
+    }
+    const badges = {
+      'MIT': '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+      'Apache 2.0': '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)',
+      'GPL 3.0': '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+      'BSD 3-Clause': '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)'
+    };
+    return badges[license] || '';
+  }
+  
+  // TODO: Create a function that returns the license link
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    if (!license) {
+      return '';
+    }
+    const links = {
+      'MIT': 'https://opensource.org/licenses/MIT',
+      'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+      'GPL 3.0': 'https://www.gnu.org/licenses/gpl-3.0',
+      'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause'
+    };
+    return links[license] || '';
+  }
+  
+  // TODO: Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {}
+  
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data) {
+    return `# ${data.title}
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
