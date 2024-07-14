a1df6a2bb33ab25a968bafed19fb29dbed70c53d
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-07-14 16:20:27 -0600

    finished functions in generateMarkdown, and questions for node.js to generate README.

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -30,12 +30,57 @@ function renderLicenseBadge(license) {
   
   // TODO: Create a function that returns the license section of README
   // If there is no license, return an empty string
-  function renderLicenseSection(license) {}
+  function renderLicenseSection(license) {
+    if (!license) {
+      return '';
+    }
+    return `## License
+  
+  This application is covered under the [${license}](${renderLicenseLink(license)}) license.`;
+  }
   
   // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
     return `# ${data.title}
-  
+    ${renderLicenseBadge(data.license)}
+
+    ## Description
+    
+    ${data.description}
+    
+    ## Table of Contents
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    
+    ${data.installation}
+    
+    ## Usage
+    
+    ${data.usage}
+    
+    ${renderLicenseSection(data.license)}
+    
+    ## Contributing
+    
+    ${data.contributing}
+    
+    ## Tests
+    
+    ${data.tests}
+    
+    ## Questions
+    
+    For any questions, please contact me with the information below:
+    
+    GitHub: [${data.github}](https://github.com/${data.github})  
+    Email: ${data.email}
   `;
   }
   
